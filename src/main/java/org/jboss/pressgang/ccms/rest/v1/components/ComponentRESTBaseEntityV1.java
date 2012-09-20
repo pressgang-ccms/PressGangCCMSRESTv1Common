cce287736857d0fdde45ddac7a3fd486be807cf4
Matthew Casperson <mcaspers@redhat.com>
2012-09-20 10:54:35 +1000

    I forgot what I did here

--- src/main/java/org/jboss/pressgang/ccms/rest/v1/components/ComponentRESTBaseEntityV1.java
+++ src/main/java/org/jboss/pressgang/ccms/rest/v1/components/ComponentRESTBaseEntityV1.java
@@ -2,6 +2,7 @@ package org.jboss.pressgang.ccms.rest.v1.components;
 
 import java.util.List;
 
+import org.jboss.pressgang.ccms.rest.v1.collections.base.RESTBaseCollectionItemV1;
 import org.jboss.pressgang.ccms.rest.v1.entities.base.RESTBaseEntityV1;
 
 @SuppressWarnings({"rawtypes", "unused"})
@@ -22,5 +23,15 @@ public class ComponentRESTBaseEntityV1
 		}
 		
 		return false;
+	}
+	
+	static public boolean returnDirtyStateForCollectionItems(final List<? extends RESTBaseCollectionItemV1> list)
+	{
+	       for (final RESTBaseCollectionItemV1 entity : list)
+	        {
+	            if (entity.getItem().returnDirtyState()) return true;
+	        }
+	        
+	        return false;
 	}
 }
