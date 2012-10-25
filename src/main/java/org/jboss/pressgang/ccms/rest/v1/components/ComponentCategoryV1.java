ca6d3e759c80505274a8daf88861544ec5ddfc63
mcasperson <matthewcasperson@gmail.com>
2012-10-25 12:49:12 +1000

    Added another utility methods

--- src/main/java/org/jboss/pressgang/ccms/rest/v1/components/ComponentCategoryV1.java
+++ src/main/java/org/jboss/pressgang/ccms/rest/v1/components/ComponentCategoryV1.java
@@ -2,6 +2,7 @@ package org.jboss.pressgang.ccms.rest.v1.components;
 
 import org.jboss.pressgang.ccms.rest.v1.collections.items.join.RESTTagInCategoryCollectionItemV1;
 import org.jboss.pressgang.ccms.rest.v1.entities.RESTCategoryV1;
+import org.jboss.pressgang.ccms.rest.v1.entities.RESTTagV1;
 import org.jboss.pressgang.ccms.rest.v1.entities.join.RESTTagInCategoryV1;
 
 public class ComponentCategoryV1
@@ -20,22 +21,32 @@ public class ComponentCategoryV1
 	
 	static public boolean containsTag(final RESTCategoryV1 source, final Integer tagId)
 	{
-		if (source == null) throw new IllegalArgumentException("source cannot be null");
-		
-		/* Strictly speaking a category can not be a parent to a tag with no id */
-		if (tagId == null)
-		    return false;
-		
-		if (source.getTags() != null && source.getTags().getItems() != null)
-		{
-			for (final RESTTagInCategoryCollectionItemV1 tagItem : source.getTags().getItems())
-			{
-			    final RESTTagInCategoryV1 tag = tagItem.getItem();
-				if (tag != null && tagId.equals(tag.getId()) && !tagItem.returnIsRemoveItem())
-					return true;
-			}
-		}
-
-		return false;
+		return returnTag(source, tagId) != null;
 	}
+	
+	public RESTTagInCategoryV1 returnTag(final Integer tagId)
+	{
+	    return returnTag(source, tagId);
+	}
+	
+	static public RESTTagInCategoryV1 returnTag(final RESTCategoryV1 source, final Integer tagId)
+    {
+        if (source == null) throw new IllegalArgumentException("source cannot be null");
+        
+        /* Strictly speaking a category can not be a parent to a tag with no id */
+        if (tagId == null)
+            return null;
+        
+        if (source.getTags() != null && source.getTags().getItems() != null)
+        {
+            for (final RESTTagInCategoryCollectionItemV1 tagItem : source.getTags().getItems())
+            {
+                final RESTTagInCategoryV1 tag = tagItem.getItem();
+                if (tag != null && tagId.equals(tag.getId()) && !tagItem.returnIsRemoveItem())
+                    return tag;
+            }
+        }
+
+        return null;
+    }
 }
