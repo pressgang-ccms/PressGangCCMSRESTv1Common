44a47115a1c2e4aadfa9eca721f695d21c23c661
Matthew Casperson <matthewcasperson@gmail.com>
2013-02-25 18:07:39 +1000

    Added the ComponentPropertyTagV1 class

---
+++ src/main/java/org/jboss/pressgang/ccms/rest/v1/components/ComponentPropertyTagV1.java
@@ -0,0 +1,39 @@
+package org.jboss.pressgang.ccms.rest.v1.components;
+
+import org.jboss.pressgang.ccms.rest.v1.collections.items.RESTTagCollectionItemV1;
+import org.jboss.pressgang.ccms.rest.v1.collections.items.join.RESTPropertyCategoryInPropertyTagCollectionItemV1;
+import org.jboss.pressgang.ccms.rest.v1.entities.RESTProjectV1;
+import org.jboss.pressgang.ccms.rest.v1.entities.RESTPropertyTagV1;
+import org.jboss.pressgang.ccms.rest.v1.entities.RESTTagV1;
+import org.jboss.pressgang.ccms.rest.v1.entities.join.RESTPropertyCategoryInPropertyTagV1;
+
+/**
+ * Contains useful functions that run against a RESTPropertyTagV1, but which are not included in the RESTPropertyTagV1
+ * class to prevent possible contamination with non GWT compatible code.
+ */
+public final class ComponentPropertyTagV1 {
+    final RESTPropertyTagV1 source;
+
+    public ComponentPropertyTagV1(final RESTPropertyTagV1 source) {
+        this.source = source;
+    }
+
+    public boolean containsTag(final Integer tagId) {
+        return isInCategory(source, tagId);
+    }
+
+    public static boolean isInCategory(final RESTPropertyTagV1 source, final Integer childId) {
+        if (source == null) throw new IllegalArgumentException("source cannot be null");
+
+        if (childId == null) return false;
+
+        if (source.getPropertyCategories() != null && source.getPropertyCategories().getItems() != null) {
+            for (final RESTPropertyCategoryInPropertyTagCollectionItemV1 childItem : source.getPropertyCategories().getItems()) {
+                final RESTPropertyCategoryInPropertyTagV1 child = childItem.getItem();
+                if (child != null && childId.equals(child.getId()) && !childItem.returnIsRemoveItem()) return true;
+            }
+        }
+
+        return false;
+    }
+}
