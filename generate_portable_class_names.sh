3f5df73ad0d6898c601c1bc757a0e37d59e32f98
Matthew Casperson <matthewcasperson@gmail.com>
2013-03-07 12:46:56 +1000

    Updated script to generate portable class names for the content spec classes

--- generate_portable_class_names.sh
+++ generate_portable_class_names.sh
@@ -8,13 +8,15 @@ COLLECTIONS=${BASE_PATH}org/jboss/pressgang/ccms/rest/v1/collections/*
 COLLECTIONS_ITEMS=${BASE_PATH}org/jboss/pressgang/ccms/rest/v1/collections/items/*
 COLLECTIONS_ITEMS_JOIN=${BASE_PATH}org/jboss/pressgang/ccms/rest/v1/collections/items/join/*
 COLLECTIONS_JOIN=${BASE_PATH}org/jboss/pressgang/ccms/rest/v1/collections/join/*
+COLLECTIONS_CONTENTSPEC=${BASE_PATH}org/jboss/pressgang/ccms/rest/v1/collections/contentspec/*
 ENTITIES=${BASE_PATH}org/jboss/pressgang/ccms/rest/v1/entities/*
+ENTITIES_CONTENTSPEC=${BASE_PATH}org/jboss/pressgang/ccms/rest/v1/entities/contentspec*
 ENTITIES_ENUMS=${BASE_PATH}org/jboss/pressgang/ccms/rest/v1/entities/enums/*
 ENTITIES_JOIN=${BASE_PATH}org/jboss/pressgang/ccms/rest/v1/entities/join/*
 
 PORTABLE_CLASSES=""
 
-for package in ${COLLECTIONS} ${COLLECTIONS_ITEMS} ${COLLECTIONS_JOIN} ${ENTITIES} ${ENTITIES_ENUMS} ${ENTITIES_JOIN} ${COLLECTIONS_ITEMS_JOIN}
+for package in ${COLLECTIONS} ${COLLECTIONS_ITEMS} ${COLLECTIONS_JOIN} ${ENTITIES} ${ENTITIES_ENUMS} ${ENTITIES_JOIN} ${COLLECTIONS_ITEMS_JOIN} ${COLLECTIONS_CONTENTSPEC} ${ENTITIES_CONTENTSPEC}
 do
 	for file in $package
 	do		
